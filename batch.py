01b2a01fb3dda389d4c0b912074e2b7f7da028ba
Gitar101 <163997614+Gitar101@users.noreply.github.com>
2024-09-02 22:07:32 +0530

    Fixed prompting

--- batch.py
+++ batch.py
@@ -423,6 +423,6 @@ if __name__ == '__main__':
     try:
         with app.app_context():
             db.create_all()
-        app.run(host='0.0.0.0', port ='5000', debug=True)
+        app.run(host='0.0.0.0', port ='7000', debug=True)
     finally:
         asyncio.get_event_loop().close()
